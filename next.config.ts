c26ceaa3ea3cfb726f508c87563b306fc402b875
peter_vida <csandragupta@gmail.com>
2025-02-14 16:37:37 +0100

    eploy-test-app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverRuntimeConfig: {
+    PORT: process.env.PORT || 3000,
+    HOST: '0.0.0.0',
+  },
 };
 
 export default nextConfig;
